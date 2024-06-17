d710833e079dd8dacfb4345d0df30a08ca1f3074
idohyeon <ehgus5026@naver.com>
2024-06-18 00:46:33 +0900

    -Entity

--- src/main/java/Study/data_jpa/entity/Member.java
+++ src/main/java/Study/data_jpa/entity/Member.java
@@ -1,27 +1,49 @@
 package Study.data_jpa.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED) // 엔티티는 JPA 스펙상 기본 생성자가 있어야 함.
+@ToString(of = {"id", "username", "age"})
+//@NamedQuery(    // 실무에서 거의 안씀.
+//        name="Member.findByUsername", // 아무렇게 줘도 됨, 관례상 이렇게 줬음.
+//        query="select m from Member m where m.username = :username")
 public class Member {
 
     @Id
     @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
-
     private String username;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     public Member(String username) {
         this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.members.add(this);
     }
 
     // @Setter 말고 이런 식으로
---
+++ src/main/java/Study/data_jpa/entity/Team.java
@@ -0,0 +1,31 @@
+package Study.data_jpa.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "name"})
+public class Team {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "team_id")
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    List<Member> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+
+}
+

94b4bdcfb1527c484c1dad7c15123a734afbca03
idohyeon <ehgus5026@naver.com>
2024-06-18 00:46:22 +0900

    -Setting

--- src/main/java/Study/data_jpa/DataJpaApplication.java
+++ src/main/java/Study/data_jpa/DataJpaApplication.java
@@ -2,8 +2,14 @@ package Study.data_jpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+/**
+ * // @EnableJpaRepositories(basePackages = "study.datajpa.repository")
+ * 스프링 부트 사용시 @SpringBootApplication 위치를 지정(해당 패키지와 하위 패키지 인식)
+ * 만약 위치가 달라지면 @EnableJpaRepositories 필요
+ */
 public class DataJpaApplication {
 
 	public static void main(String[] args) {

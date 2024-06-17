33b856a579e5cf48d1b71e96fc265e57bb657dc7
idohyeon <ehgus5026@naver.com>
2024-06-17 19:09:06 +0900

    -Setting

--- src/test/java/Study/data_jpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/Study/data_jpa/repository/MemberJpaRepositoryTest.java
@@ -5,6 +5,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.*;
@@ -19,14 +20,16 @@ class MemberJpaRepositoryTest {
 
     @Test
     @Transactional
+    @Rollback(value = false)
     public void testMember() {
         Member member = new Member("memberA");
         Member savedMember = memberJpaRepository.save(member);
 
-        Member findMember = memberJpaRepository.find(member.getId());
+        Member findMember = memberJpaRepository.find(savedMember.getId());
 
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member);
     }
 
 }

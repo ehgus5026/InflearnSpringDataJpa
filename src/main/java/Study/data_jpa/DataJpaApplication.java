527c8affb143b4822fc493e9db7fd2ca313de7d5
idohyeon <ehgus5026@naver.com>
2024-06-18 23:52:30 +0900

    -Auditing Setting

--- src/main/java/Study/data_jpa/DataJpaApplication.java
+++ src/main/java/Study/data_jpa/DataJpaApplication.java
@@ -2,7 +2,13 @@ package Study.data_jpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @SpringBootApplication
 /**
@@ -10,10 +16,16 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  * 스프링 부트 사용시 @SpringBootApplication 위치를 지정(해당 패키지와 하위 패키지 인식)
  * 만약 위치가 달라지면 @EnableJpaRepositories 필요
  */
+@EnableJpaAuditing // Spring Data Jpa가 지원해주는 Auditing
 public class DataJpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DataJpaApplication.class, args);
+	}
+
+	@Bean
+	public AuditorAware<String> auditorProvider() {
+		return () -> Optional.of(UUID.randomUUID().toString());
 	}
 
 }

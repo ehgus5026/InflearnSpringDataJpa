1fef64a7dd5a8f8149e080991318ba1b92b371b4
idohyeon <ehgus5026@naver.com>
2024-06-18 23:51:11 +0900

    -MemberView DTO

--- src/main/java/Study/data_jpa/dto/MemberDto.java
+++ src/main/java/Study/data_jpa/dto/MemberDto.java
@@ -1,6 +1,7 @@
 package Study.data_jpa.dto;
 
 
+import Study.data_jpa.entity.Member;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -11,5 +12,12 @@ public class MemberDto {
     private Long id;
     private String username;
     private String teamName;
+
+    // 엔티티는 DTO를 바라보면 안되지만, DTO는 엔티티를 바라봐도 됨.
+    public MemberDto(Member member) {
+        this.id = member.getId();
+        this.username = member.getUsername();
+        this.teamName = member.getTeam().getName();
+    }
 
 }

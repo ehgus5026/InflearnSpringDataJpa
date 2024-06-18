5fd6e9ae70711a85850226f90c6ce7b25e689620
idohyeon <ehgus5026@naver.com>
2024-06-18 23:53:08 +0900

    -Test

--- src/test/java/Study/data_jpa/entity/MemberTest.java
+++ src/test/java/Study/data_jpa/entity/MemberTest.java
@@ -1,8 +1,10 @@
 package Study.data_jpa.entity;
 
+import Study.data_jpa.repository.MemberRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,6 +20,9 @@ class MemberTest {
 
     @PersistenceContext
     EntityManager em;
+
+    @Autowired
+    MemberRepository memberRepository;
 
     @Test
     @Transactional
@@ -48,4 +53,28 @@ class MemberTest {
             System.out.println("-> member.team = " + member.getTeam());
         }
     }
+
+    @Test
+    public void JpaEventBaseEntity() throws Exception {
+        // given
+        Member member = new Member("member1");
+        memberRepository.save(member); // @PrePersist
+
+        Thread.sleep(100);
+        member.setUsername("member2");
+
+        em.flush(); // @PreUpdate
+        em.clear();
+
+        // when
+        Member findMember = memberRepository.findById(member.getId()).get();
+
+        // then
+        System.out.println("findMember.getCreatedDate = " + findMember.getCreatedDate());
+        System.out.println("findMember.getUpdatedDate = " + findMember.getLastModifiedDate());
+//        System.out.println("findMember.getUpdatedDate = " + findMember.getUpdatedDate());
+        System.out.println("findMember.getCreatedBy = " + findMember.getCreatedBy());
+        System.out.println("findMember.getLastModifiedBy = " + findMember.getLastModifiedBy());
+    }
+
 }
--- src/test/java/Study/data_jpa/repository/MemberRepositoryTest.java
+++ src/test/java/Study/data_jpa/repository/MemberRepositoryTest.java
@@ -3,9 +3,15 @@ package Study.data_jpa.repository;
 import Study.data_jpa.dto.MemberDto;
 import Study.data_jpa.entity.Member;
 import Study.data_jpa.entity.Team;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,6 +19,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,6 +29,7 @@ class MemberRepositoryTest {
 
     @Autowired MemberRepository memberRepository;
     @Autowired TeamRepository teamRepository;
+    @PersistenceContext EntityManager em;
 
     @Test
     @Transactional
@@ -140,5 +148,140 @@ class MemberRepositoryTest {
         System.out.println("aaa2 = " + aaa2); // Optional.empty(null 이면)
     }
 
+    @Test
+    @Transactional
+    public void paging() {
+        //given
+        memberRepository.save(new Member("member1", 10));
+        memberRepository.save(new Member("member2", 10));
+        memberRepository.save(new Member("member3", 10));
+        memberRepository.save(new Member("member4", 10));
+        memberRepository.save(new Member("member5", 10));
+
+        int age = 10;
+        PageRequest pageRequest = PageRequest.of(0, 3, Sort.Direction.DESC, "username"); // 0페이지에서 3개 가져오면서 username을 내림차순 정렬
+        
+        //when
+        Page<Member> page1 = memberRepository.findByAge(age, pageRequest);
+        Page<MemberDto> toDto = page1.map(m -> new MemberDto(m.getId(), m.getUsername(), null));
+//        Slice<Member> page2 = memberRepository.findByAge(age, pageRequest);
+
+        //then
+//        List<Member> content = page1.getContent(); // 페이징한 실제 데이터 꺼내기(조회된 데이터)
+//        long totalElements = page1.getTotalElements();
+//        for (Member member : content) {
+//            System.out.println("member = " + member);
+//        }
+//        System.out.println("totalElements = " + totalElements);
+
+        // Page
+        assertThat(page1.getContent().size()).isEqualTo(3); //조회된 데이터
+        assertThat(page1.getTotalElements()).isEqualTo(5); //전체 데이터 수
+        assertThat(page1.getNumber()).isEqualTo(0); //페이지 번호
+        assertThat(page1.getTotalPages()).isEqualTo(2); //전체 페이지 번호
+        assertThat(page1.isFirst()).isTrue(); //첫번째 항목인가?
+        assertThat(page1.hasNext()).isTrue(); //다음 페이지가 있는가?
+
+        // Slice
+//        assertThat(page2.getContent().size()).isEqualTo(3); //조회된 데이터
+//        assertThat(page2.getNumber()).isEqualTo(0); //페이지 번호
+//        assertThat(page2.isFirst()).isTrue(); //첫번째 항목인가?
+//        assertThat(page2.hasNext()).isTrue(); //다음 페이지가 있는가?
+    }
+
+    @Test
+    @Transactional
+    @Rollback(value = false)
+    public void bulkUpdate() {
+        // given
+        memberRepository.save(new Member("member1", 10));
+        memberRepository.save(new Member("member2", 19));
+        memberRepository.save(new Member("member3", 20));
+        memberRepository.save(new Member("member4", 21));
+        memberRepository.save(new Member("member5", 40));
+
+        // when
+        int resultCount = memberRepository.bulkAgePlus(20);
+//        em.flush();
+//        em.clear();
+
+        List<Member> result = memberRepository.findByUsername("member5");
+        Member member5 = result.get(0);
+        // 벌크 연산을 때리면 영속성 컨텍스트를 무시하고 디비에 바로 쿼리 날려버림.
+        // 그래서 @Modifying(clearAutomatically = true) 이걸 안 키면, 영속성 컨텍스트는 40살로 남아있고, 디비엔 41살로 되어 있음.
+        System.out.println("member5 = " + member5);
+
+        // then
+        assertThat(resultCount).isEqualTo(3);
+    }
+
+    @Test
+    @Transactional
+    @Rollback(value = false)
+    public void findMemberLazy() {
+        // given
+        // member1 -> teamA
+        // member2 -> teamB
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        teamRepository.save(teamA);
+        teamRepository.save(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamB);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        em.flush();
+        em.clear();
+
+        // when N + 1
+        // select Member 1
+//        List<Member> members = memberRepository.findAll(); // member만 select
+        List<Member> members = memberRepository.findEntityGraphByUsername("member1");
+//        List<Member> members = memberRepository.findMemberFetchJoin();
+
+        for (Member member : members) {
+            System.out.println("member = " + member); // member 이미 select 쳐놔서 그냥 가져옴.
+            System.out.println("member = " + member.getTeam().getName()); // 이때 team 지연 로딩 걸려 있던 거에서 실데이터 값 가지고 옴.
+        }
+    }
+
+    @Test
+    @Transactional
+    @Rollback(value = false)
+    public void queryHint() {
+        // given
+        Member member1 = memberRepository.save(new Member("member1", 10));
+        em.flush();
+        em.clear();
+
+        // when
+//        Member findMember = memberRepository.findById(member1.getId()).get();
+        Member findMember = memberRepository.findReadOnlyByUsername("member1");
+        findMember.setUsername("member2"); // findReadOnlyByUsername() 에서 읽기 전용으로 해놔서 변경 감지 X
+
+        em.flush();
+    }
+
+    @Test
+    @Transactional
+    @Rollback(value = false)
+    public void lock() {
+        // given
+        Member member1 = memberRepository.save(new Member("member1", 10));
+        em.flush();
+        em.clear();
+
+        // when
+        List<Member> result = memberRepository.findLockByUsername("member1");
+
+    }
+
+    @Test
+    @Transactional
+    public void callCustom() {
+        List<Member> result = memberRepository.findMemberCustom();
+    }
 
 }
